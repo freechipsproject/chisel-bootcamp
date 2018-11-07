531947a3e568ac3bbd4b4670b41420b83312d6c1
Paul Rigge <rigge@berkeley.edu>
2018-11-07 19:25:16 +0000

    Add initial attempt at travis support

---
+++ .travis_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Jupyter
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin":$PATH
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda install jupyter
+
+# Almond (Scala kernel for Jupyter)
+git clone -b v0.1.9 https://github.com/almond-sh/almond.git
+cd almond
+curl -L -o coursier https://git.io/coursier && chmod +x coursier
+./coursier bootstrap \
+    -i user -I user:sh.almond:scala-kernel-api_$SCALA_VERSION:$ALMOND_VERSION \
+    sh.almond:scala-kernel_$SCALA_VERSION:$ALMOND_VERSION \
+    -o almond
+./almond --install
+
+# Copy custom css
+mkdir -p ~/.jupyter/custom
+cp source/custom.js ~/.jupyter/custom/custom.js
+
